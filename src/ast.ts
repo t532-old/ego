688d50b32d489c687cf858f2feaeae6c9f1b0001
t532 <2037246484@qq.com>
2019-07-08 04:32:47 +0800

    Add line numbers to Tokenizer and AST Parser

--- src/ast.ts
+++ src/ast.ts
@@ -1,41 +1,47 @@
+import { Tokenizer } from './tokenizer'
+
 export namespace AST {
     const LPAREN = '(',
         RPAREN = ')'
-    export namespace Types {
-        export interface CallNode {
-            type: 'FunctionCall'
-            target: Node
-            args: Node[]
-        }
-        export function Call(target: Node, args: Node[]): CallNode {
-            return {
-                type: 'FunctionCall',
-                target,
-                args,
-            }
-        }
-        export interface IdentifierNode {
-            type: 'Identifier'
-            name: string
-        }
-        export function Identifier(name: string): IdentifierNode {
-            return {
-                type: 'Identifier',
-                name,
-            }
-        }
-        export type Node = CallNode | IdentifierNode
+    export interface CallNode {
+        type: 'FunctionCall'
+        target: Node
+        args: Node[]
     }
-    export function generate(tokens: string[], isRoot = true) {
-        const forest: Types.Node[] = []
-        let last: Types.Node = {
+    export function Call(target: Node, args: Node[]): CallNode {
+        return {
+            type: 'FunctionCall',
+            target,
+            args,
+        }
+    }
+    export interface IdentifierNode {
+        type: 'Identifier'
+        name: string
+        row: number
+        col: number
+    }
+    export function Identifier(name: string, row: number, col: number): IdentifierNode {
+        return {
+            type: 'Identifier',
+            name,
+            row,
+            col,
+        }
+    }
+    export type Node = CallNode | IdentifierNode
+    export function generate(tokens: Tokenizer.Token[], isRoot = true) {
+        const forest: Node[] = []
+        let last: Node = {
             type: 'Identifier',
             name: 'null',
+            row: 0,
+            col: 0,
         }
-        if (isRoot) tokens.push(')')
+        if (isRoot) tokens.push({ token: ')', row: Infinity, col: Infinity })
         for (let idx = 0; idx < tokens.length; idx++) {
             const raw = tokens[idx]
-            if (raw === RPAREN) {
+            if (raw.token === RPAREN) {
                 if (isRoot && idx !== tokens.length - 1)
                     throw new Error('Parentheses unmatched')
                 return {
@@ -43,14 +49,14 @@ export namespace AST {
                     idx,
                 }
             }
-            if (raw === LPAREN) {
+            if (raw.token === LPAREN) {
                 forest.pop()
                 const children = generate(tokens.slice(idx + 1), false)
-                const call = last = Types.Call(last, children.forest)
+                const call = last = Call(last, children.forest)
                 idx += children.idx + 1
                 forest.push(call)
             } else {
-                const self = last = Types.Identifier(raw)
+                const self = last = Identifier(raw.token, raw.row, raw.col)
                 forest.push(self)
             }
         }
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,34 +1,45 @@
 export namespace Tokenizer {
-    export namespace REGEX {
-        export const WHITESPACE = /\s/,
-            STRING_DELIM = /"/,
-            STRING_REG = /\\/,
-            PAREN = /\(|\)/
+    const WHITESPACE = /\s/,
+        STRING_DELIM = /"/,
+        STRING_REG = /\\/,
+        PAREN = /\(|\)/
+    export interface Token {
+        token: string
+        row: number
+        col: number
     }
     export function tokenize(raw: string) {
-        const tokens = ['']
         const last = () => tokens.length - 1
         let inStr = false, strReg = false
+        let row = 1, col = 1
+        const tokens: Token[] = [{ token: '', row, col }]
         for (const chr of raw) {
+            if (!tokens[last()].token.length)
+                tokens[last()] = { token: '', row, col }
             if (inStr) {
                 if (strReg) {
-                    tokens[last()] += chr
+                    tokens[last()].token += chr
                     strReg = false
-                } else if (REGEX.STRING_REG.test(chr))
+                } else if (STRING_REG.test(chr))
                     strReg = true
-                else if (REGEX.STRING_DELIM.test(chr))
+                else if (STRING_DELIM.test(chr))
                     inStr = false
-                else tokens[last()] += chr
+                else tokens[last()].token += chr
             } else {
-                if (REGEX.WHITESPACE.test(chr)) {
-                    if (tokens[last()].length) tokens.push('')
-                } else if (REGEX.STRING_DELIM.test(chr))
+                if (WHITESPACE.test(chr)) {
+                    if (tokens[last()].token.length) tokens.push({ token: '', row, col })
+                } else if (STRING_DELIM.test(chr))
                     inStr = true
-                else if (REGEX.PAREN.test(chr))
-                    tokens.push(chr, '')
-                else tokens[last()] += chr
+                else if (PAREN.test(chr))
+                    tokens.push({ token: chr, row, col }, { token: '', row, col })
+                else tokens[last()].token += chr
+            }
+            col++
+            if (chr === '\n') {
+                col = 1
+                row++
             }
         }
-        return tokens.filter(i => i.length)
+        return tokens.filter(i => i.token.length)
     }
 }
