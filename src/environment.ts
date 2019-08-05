21c44f7735f051946937c673e22d6a2912892099
t532 <2037246484@qq.com>
2019-08-05 22:18:53 +0800

    Throw Exception when trying to read/write an undeclared variable

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,6 @@
 import { Value, Null } from './types'
 import { Executable } from './executor'
+import { InternalException } from './throwable'
 
 export class Variable {
     readonly name: string
@@ -9,6 +10,15 @@ export class Variable {
         this.name = name
         this.value = value
         this.scope = scope
+    }
+}
+export class DummyVariable implements Variable {
+    readonly name: string
+    get value() { throw new InternalException('Cannot get value of undeclared variable') }
+    set value(_: Value) { throw new InternalException('Cannot set value of undeclared variable') }
+    readonly scope: Scope = null
+    constructor(name: string) {
+        this.name = name
     }
 }
 export class Scope {
@@ -24,7 +34,7 @@ export class Scope {
         else if (this.parent)
             return this.parent.get(name)
         else
-            return new Variable(name, Null(), this)
+            return new DummyVariable(name)
     }
     call(name: string, args: Executable[], parent: Scope) { return this.get(name).value.call(args, parent) }
 }
